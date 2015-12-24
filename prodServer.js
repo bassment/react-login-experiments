93d8e5876e557c2972824492f57b65651e4b761a
codterpin <anton.perebyinis@gmail.com>
2015-12-24 12:12:21 -0500

    Fix production version

--- prodServer.js
+++ prodServer.js
@@ -5,9 +5,10 @@ var app = express();
 var port = 3000;
 var domain = 'pixelant.space';
 
+app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/public'));
 
-app.get('api', function (req, res) {
+app.get('api', function(req, res) {
   res.send('API express route...');
 });
 
@@ -19,5 +20,6 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   }
+
   console.info("==> 🌎 Listening on port %s. Open up http:%s in your browser.", port, domain);
 });
